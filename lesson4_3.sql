670c19548e277a484abebba2a2300597023e7ed3
LeeCiacia <thaicianilee@gmail.com>
2024-03-02 15:47:34 +0800

    Update lesson4_3.sql

--- lesson4_3.sql
+++ lesson4_3.sql
@@ -42,3 +42,50 @@ SELECT *
 FROM student
 ORDER BY score
 LIMIT 2;
+
+SELECT *
+FROM student
+WHERE major = '英語'
+ORDER BY score DESC
+LIMIT 1
+
+SELECT *
+FROM student
+WHERE major = '英語' AND score < 60
+
+SELECT *
+FROM student
+WHERE major = '英語' OR score > 60
+
+SELECT *
+FROM student
+WHERE major = '英語' OR major = '生物' OR major = '歷史'
+
+/*把OR改寫成IN*/
+SELECT *
+FROM student
+WHERE major IN('英語','生物','歷史')
+
+SELECT *
+FROM student
+WHERE score >= 80 AND score <= 100
+
+/*可以把上面的改寫成between*/
+SELECT *
+FROM student
+WHERE score between 80 AND 100
+
+SELECT *
+FROM student
+WHERE name ='小黃'
+
+SELECT *
+FROM student
+WHERE NOT name ='小黃'
+
+
+SELECT *
+FROM student
+WHERE name LIKE '%黃%'
+
+
