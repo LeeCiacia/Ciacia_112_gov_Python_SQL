31b20d4f212845277ac831bc4e3b784a86391722
LeeCiacia <thaicianilee@gmail.com>
2024-03-09 13:31:18 +0800

    Update lesson5_1.sql

--- lesson5_1.sql
+++ lesson5_1.sql
@@ -1,5 +1,5 @@
- DROP TABLE IF EXISTS 聯絡人;
- DROP TABLE IF EXISTS 客戶;
+DROP TABLE IF EXISTS 聯絡人;
+DROP TABLE IF EXISTS 客戶;
 
 CREATE TABLE IF NOT EXISTS 客戶(
 	客戶_id SERIAL,
@@ -7,13 +7,25 @@ CREATE TABLE IF NOT EXISTS 客戶(
 	PRIMARY KEY(客戶_id)
  );
  
- CREATE TABLE IF NOT EXISTS 聯絡人(
+CREATE TABLE IF NOT EXISTS 聯絡人(
 	 聯絡人id SERIAL,
 	 客戶id INT,
-	 聯絡姓名 VARCHAR(255) NOT NULL,
+	 聯絡人姓名 VARCHAR(255) NOT NULL,
 	 電話 VARCHAR(15),
 	 郵件 VARCHAR(100),
 	 PRIMARY KEY(聯絡人id),
 	 FOREIGN KEY(客戶id) REFERENCES 客戶(客戶_id)
+	 ON DELETE SET NULL
  );
  
+INSERT INTO 客戶(客戶名稱)
+VALUES('遠傳電信'),
+	 ('台灣大車隊');
+	 
+INSERT INTO 聯絡人(客戶id,聯絡人姓名,電話,郵件)
+VALUES (1,'John Doe','(408)-111-1234','john.doe@bluebird.dev'),
+	   (1,'Jane Doe','(408)-111-1235','jane.doe@bluebird.dev'),
+	   (2,'David Wright','(408)-222-1234','david.wright@dolphin.dev');
+
+DELETE FROM 客戶
+WHERE 客戶_id=1
