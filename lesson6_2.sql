c30bf7e95e71ae160c5ead2c1c830d0300d21c1c
LeeCiacia <thaicianilee@gmail.com>
2024-03-09 15:34:43 +0800

    Update lesson6_2.sql

--- lesson6_2.sql
+++ lesson6_2.sql
@@ -22,5 +22,17 @@ WHERE 日期 = '2022-03-01'AND 名稱 = '基隆'
 /*取出基隆火車站2022年3月的資料，時間由小到大*/
 SELECT DISTINCT 日期,進站人數,出站人數,名稱,地名,地址,youbike
 FROM gate_count LEFT JOIN stations ON 站點編號=編號
-WHERE (日期 BETWEEN '2022-03-01' AND '2022-03-31') AND 名稱 = '基隆'
+WHERE (日期 BETWEEN '2022-03-01' AND '2022-03-31') AND 名稱 '基隆'
 ORDER BY 日期 ASC;
+
+/*取出基隆火車站和臺北火車站2022年3月的資料，時間由小到大*/
+SELECT DISTINCT 日期,進站人數,出站人數,名稱,地名,地址,youbike
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+WHERE (日期 BETWEEN '2022-03-01' AND '2022-03-31') AND 名稱 IN('基隆','臺北')
+ORDER BY 日期 ASC;
+
+/*取出資料進站人數最多的前10筆資料*/
+SELECT DISTINCT 日期,進站人數,出站人數,名稱,地名,地址,youbike
+FROM gate_count LEFT JOIN stations ON 站點編號=編號
+ORDER BY 進站人數 DESC
+LIMIT 10;
