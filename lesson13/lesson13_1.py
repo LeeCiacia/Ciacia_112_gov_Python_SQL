2e52b61c0cedbb6d715093425d1c7e733ee9db1f
LeeCiacia <thaicianilee@gmail.com>
2024-04-14 02:21:42 +0800

    new lessons

--- lesson13/lesson13_1.py
+++ lesson13/lesson13_1.py
@@ -10,7 +10,8 @@ with psycopg2.connect(os.environ['POSTGRE_PASSWORD']) as conn:
             FROM 聯絡人 LEFT JOIN 客戶 ON 聯絡人.客戶id = 客戶.客戶_id
         '''
         cursor.execute(sql)
-        datas:list  = cursor.fetchmany(10) #10表示10筆資料，看要取出幾筆(但這次資料只有3筆，所以只會出現3筆) 
+        datas:list = cursor.fetchmany(10)
+        
 
 for item in datas:
     print(item)
