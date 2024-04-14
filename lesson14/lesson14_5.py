ba9667f91690dcc1b62403a33fc90587c641b7a2
LeeCiacia <thaicianilee@gmail.com>
2024-04-14 23:33:02 +0800

    new lesson

--- lesson14/lesson14_5.py
+++ lesson14/lesson14_5.py
@@ -6,8 +6,8 @@ import psycopg2
 import os
 load_dotenv()
 
-#自訂function功能def get_contact():
-def get_contacts() -> list:
+@st.cache_resource #加這個效能好
+def get_contacts() -> list:  #自訂function功能def get_contact():
     with psycopg2.connect(os.environ['POSTGRE_PASSWORD']) as conn:
         with conn.cursor() as cursor:
             sql='''
@@ -20,12 +20,27 @@ def get_contacts() -> list:
             for item in datas:
                 contacts.append({'id':item[0],'客戶名稱':item[1],'姓名':item[2],'電話':item[3],'郵件':item[4]}) 
             return contacts  #要return 資料contacts才會傳出來
-            
+
+
+@st.cache_resource #加這個效能好
+def get_names() -> list:
+    with psycopg2.connect(os.environ['POSTGRE_PASSWORD']) as conn:
+        with conn.cursor() as cursor:
+            sql='''
+                SELECT DISTINCT 客戶名稱
+                FROM 聯絡人 LEFT JOIN 客戶 ON 聯絡人.客戶id = 客戶.客戶_id
+            '''
+            cursor.execute(sql)
+            datas:list(tuple) = cursor.fetchmany(10)
+            names=[]
+            for item in datas:
+                 names.append(item[0])
+            return(datas)
+        
 
 source_data:list[dict] = get_contacts()
-st.dataframe(source_data,width=1200)
-
-# 這些原來lesson13_1的程式碼要刪掉⬇️⬇️
-#for item in datas:
-#print(item)
+with st.sidebar:  # 這些要做成sidebar
+    option = st.selectbox(
+    '請選擇姓名:',
+    get_names())
 
